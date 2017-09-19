8724eb6e58aa924c5662b2cdc879dd83868526b3
Brian Bockelman <bbockelm@cse.unl.edu>
2017-09-18 22:10:31 -0500

    Have multiuser plugin invoke ACC if necessary.

--- src/multiuser.cpp
+++ src/multiuser.cpp
@@ -1,26 +1,54 @@
 
-#include "XrdVersion.hh"
+#include "XrdAcc/XrdAccAuthorize.hh"
+#include "XrdOuc/XrdOucEnv.hh"
+#include "XrdOuc/XrdOucPinPath.hh"
+#include "XrdOuc/XrdOucStream.hh"
 #include "XrdSec/XrdSecEntity.hh"
 #include "XrdSfs/XrdSfsInterface.hh"
+#include "XrdVersion.hh"
 
+#include <exception>
 #include <memory>
 #include <mutex>
 #include <vector>
 
+#include <dlfcn.h>
+#include <fcntl.h>
 #include <pwd.h>
 #include <sys/fsuid.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+
 XrdVERSIONINFO(XrdSfsGetFileSystem, Multiuser);
+
+// The status-quo to retrieve the default object is to copy/paste the
+// linker definition and invoke directly.
+static XrdVERSIONINFODEF(compiledVer, XrdAccTest, XrdVNUMBER, XrdVERSION);
+extern XrdAccAuthorize *XrdAccDefaultAuthorizeObject(XrdSysLogger   *lp,
+                                                     const char     *cfn,
+                                                     const char     *parm,
+                                                     XrdVersionInfo &myVer);
+
 
 class UserSentry {
 public:
-    UserSentry(const XrdSecEntity *client, XrdSysError &log) :
+    UserSentry(const XrdSecEntity *client, XrdSysError &log, XrdAccAuthorize *authz, const char *opaque) :
         m_log(log)
     {
-        if (!client) {return;}
-        if (!client->name || !client->name[0]) {return;}
+        if (!client) {
+            log.Emsg("UserSentry", "No security entity object provided");
+            return;
+        }
+        if (authz && (client->sessvar != reinterpret_cast<int *>(1)) && (!client->name || !client->name[0])) {
+            const_cast<XrdSecEntity*>(client)->sessvar = reinterpret_cast<int *>(1);
+            XrdOucEnv env(opaque, 0, client);
+            authz->Access(client, "/", AOP_Any, &env);
+        }
+        if (!client->name || !client->name[0]) {
+            log.Emsg("UserSentry", "Anonymous client; no user set, cannot change FS UIDs");
+            return;
+        }
         struct passwd pwd, *result = nullptr;
 
         // TODO: cache the lot of this.
@@ -43,6 +71,7 @@ public:
             m_log.Emsg("UserSentry", "Failed to lookup UID for username", client->name, strerror(retval));
             return;
         }
+        m_log.Emsg("UserSentry", "Switching FS uid for user", client->name);
 
         m_orig_uid = setfsuid(result->pw_uid);
         // TODO: log failures.
@@ -68,9 +97,10 @@ private:
 
 class MultiuserFile : public XrdSfsFile {
 public:
-    MultiuserFile(std::unique_ptr<XrdSfsFile> sfs, XrdSysError &log) :
+    MultiuserFile(std::unique_ptr<XrdSfsFile> sfs, XrdSysError &log, std::shared_ptr<XrdAccAuthorize> authz) :
         m_sfs(std::move(sfs)),
-        m_log(log)
+        m_log(log),
+        m_authz(authz)
     {}
 
     virtual ~MultiuserFile() {}
@@ -82,7 +112,7 @@ public:
          const XrdSecEntity       *client,
          const char               *opaque = 0) override
     {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->open(fileName, openMode, createMode, client, opaque);
     }
 
@@ -161,13 +191,15 @@ public:
 private:
     std::unique_ptr<XrdSfsFile> m_sfs;
     XrdSysError &m_log;
+    std::shared_ptr<XrdAccAuthorize> m_authz;
 };
 
 class MultiuserDirectory : public XrdSfsDirectory {
 public:
-    MultiuserDirectory(std::unique_ptr<XrdSfsDirectory> sfs, XrdSysError &log) :
+    MultiuserDirectory(std::unique_ptr<XrdSfsDirectory> sfs, XrdSysError &log, std::shared_ptr<XrdAccAuthorize> authz) :
         m_sfs(std::move(sfs)),
-        m_log(log)
+        m_log(log),
+        m_authz(authz)
     {}
 
     virtual ~MultiuserDirectory() {}
@@ -176,7 +208,7 @@ public:
     open(const char              *path,
          const XrdSecEntity      *client = 0,
          const char              *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->open(path, client, opaque);
     }
 
@@ -199,29 +231,118 @@ public:
 private:
     std::unique_ptr<XrdSfsDirectory> m_sfs;
     XrdSysError &m_log;
+    std::shared_ptr<XrdAccAuthorize> m_authz;
 };
 
 class MultiuserFileSystem : public XrdSfsFileSystem {
 public:
 
-    MultiuserFileSystem(std::unique_ptr<XrdSfsFileSystem> sfs, XrdSysLogger * lp, const char * /*configfn*/) :
-        m_sfs(std::move(sfs)),
+    MultiuserFileSystem(XrdSfsFileSystem *sfs, XrdSysLogger *lp, const char *configfn) :
+        m_sfs(sfs),
         m_log(lp, "multiuser_")
     {
+        m_log.Say("------ Initializing the multi-user plugin.");
+        if (!Config(lp, configfn)) {
+            throw std::runtime_error("Failed to configure multi-user plugin.");
+        }
     }
 
-    virtual ~MultiuserFileSystem() {}
+    virtual ~MultiuserFileSystem() {
+        if (m_handle) {
+            dlclose(m_handle);
+        }
+    }
+
+    bool
+    Config(XrdSysLogger *lp, const char *configfn)
+    {
+        XrdOucEnv myEnv;
+        XrdOucStream Config(&m_log, getenv("XRDINSTANCE"), &myEnv, "=====> ");
+
+        bool set_authorize{false};
+        std::string authLib;
+        std::string authLibParms;
+        int cfgFD = open(configfn, O_RDONLY, 0);
+        if (cfgFD < 0) {
+            m_log.Emsg("Config", errno, "open config file", configfn);
+            return false;
+        }
+        Config.Attach(cfgFD);
+        const char *val;
+        while ((val = Config.GetMyFirstWord())) {
+            if (!strcmp("ofs.authorize", val)) {
+                set_authorize = true;
+                Config.Echo();
+            }
+            else if (!strcmp("ofs.authlib", val)) {
+                val = Config.GetWord();
+                if (!val || !val[0]) {
+                    m_log.Emsg("Config", "ofs.authlib does not specify a library");
+                    Config.Close();
+                    return false;
+                }
+                authLib = val;
+                std::vector<char> rest; rest.reserve(2048);
+                if (!Config.GetRest(&rest[0], sizeof(rest))) {
+                    m_log.Emsg("Config", "Parameters line too long");
+                    Config.Close();
+                    return false;
+                }
+                if (rest[0] != '\0') {
+                    authLibParms = &rest[0];
+                }
+            }
+        }
+        int retc = Config.LastError();
+        if (retc) {
+            m_log.Emsg("Config", -retc, "read config file", configfn);
+            Config.Close();
+            return false;
+        }
+        Config.Close();
+
+        if (!set_authorize) {return true;}
+
+        XrdAccAuthorize *(*ep)(XrdSysLogger *, const char *, const char *);
+        if (authLib.empty()) {
+            m_authz.reset(XrdAccDefaultAuthorizeObject(lp, configfn, authLibParms.c_str(), compiledVer));
+        } else {
+            char resolvePath[2048];
+            bool usedAltPath{true};
+            if (!XrdOucPinPath(authLib.c_str(), usedAltPath, resolvePath, 2048)) {
+                m_log.Emsg("Config", "Failed to locate appropriately versioned authlib path for", authLib.c_str());
+                return false;
+            }
+            m_handle = dlopen(resolvePath, RTLD_LOCAL|RTLD_NOW);
+            if (m_handle == nullptr) {
+                m_log.Emsg("Config", "Failed to load", resolvePath, dlerror());
+                return false;
+            }
+            ep = (XrdAccAuthorize *(*)(XrdSysLogger *, const char *, const char *))
+                             (dlsym(m_handle, "XrdAccAuthorizeObject"));
+            if (ep) {
+                m_authz.reset(ep(lp, configfn, authLibParms.c_str()));
+            } else {
+                m_log.Emsg("Config", "Failed to resolve symbol XrdAccAuthorizeObject",dlerror());
+            }
+        }
+        if (!m_authz) {
+            m_log.Emsg("Config", "Failed to configure and load authorization plugin");
+            return false;
+        }
+        return true;
+    }
 
     virtual XrdSfsDirectory *
     newDir(char *user=nullptr, int monid=0) override {
         std::unique_ptr<XrdSfsDirectory> chained_dir(m_sfs->newDir(user, monid));
-        return new MultiuserDirectory(std::move(chained_dir), m_log);
+        return new MultiuserDirectory(std::move(chained_dir), m_log, m_authz);
     }
 
     virtual XrdSfsFile *
     newFile(char *user=0, int monid=0) override {
         std::unique_ptr<XrdSfsFile> chained_file(m_sfs->newFile(user, monid));
-        return new MultiuserFile(std::move(chained_file), m_log);
+        return new MultiuserFile(std::move(chained_file), m_log, m_authz);
     }
 
     virtual int
@@ -231,7 +352,7 @@ public:
                  XrdOucErrInfo &eInfo,
            const XrdSecEntity  *client = 0,
            const char          *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->chksum(Func, csName, path, eInfo, client, opaque);
     }
 
@@ -241,14 +362,20 @@ public:
                 XrdOucErrInfo    &out_error,
           const XrdSecEntity     *client,
           const char             *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->chmod(Name, Mode, out_error, client, opaque);
     }
 
     virtual void
     Disc(const XrdSecEntity   *client = 0) override {
-        UserSentry sentry(client, m_log);
-        return m_sfs->Disc(client);
+        // It seems that Xrootd calls this with a different object than elsewhere; to prevent
+        // log spam, add an extra check.
+        if (client && client->name) {
+            UserSentry sentry(client, m_log, m_authz.get(), nullptr);
+            return m_sfs->Disc(client);
+        } else {
+            return m_sfs->Disc(client);
+        }
     }
 
     virtual void
@@ -262,7 +389,7 @@ public:
                  XrdOucErrInfo       &out_error,
            const XrdSecEntity        *client,
            const char                *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->exists(fileName, exists_flag, out_error, client, opaque);
     }
 
@@ -271,7 +398,7 @@ public:
           const char             *args,
                 XrdOucErrInfo    &out_error,
           const XrdSecEntity     *client) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), nullptr);
         return m_sfs->fsctl(cmd, args, out_error);
     }
 
@@ -291,7 +418,7 @@ public:
                 XrdOucErrInfo    &out_error,
           const XrdSecEntity     *client,
           const char             *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->mkdir(dirName, Mode, out_error, client, opaque);
     }
 
@@ -299,7 +426,7 @@ public:
     prepare(      XrdSfsPrep       &pargs,
                   XrdOucErrInfo    &out_error,
             const XrdSecEntity     *client = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), nullptr);
         return m_sfs->prepare(pargs, out_error, client);
     }
 
@@ -308,7 +435,7 @@ public:
               XrdOucErrInfo    &out_error,
         const XrdSecEntity     *client,
         const char             *info = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), info);
         return m_sfs->rem(path, out_error, client, info);
     }
 
@@ -317,7 +444,7 @@ public:
                  XrdOucErrInfo    &out_error,
            const XrdSecEntity     *client,
            const char             *info = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), info);
         return m_sfs->remdir(dirName, out_error, client, info);
     }
 
@@ -328,7 +455,7 @@ public:
            const XrdSecEntity     *client,
            const char             *infoO = 0,
            const char             *infoN = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), infoO);
         return m_sfs->rename(oldFileName, newFileName, out_error, client, infoO, infoN);
     }
 
@@ -338,7 +465,7 @@ public:
                XrdOucErrInfo    &out_error,
          const XrdSecEntity     *client,
          const char             *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->stat(Name, buf, out_error, client, opaque);
     }
 
@@ -348,7 +475,7 @@ public:
                XrdOucErrInfo    &out_error,
          const XrdSecEntity     *client,
          const char             *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->stat(Name, mode, out_error, client, opaque);
     }
 
@@ -358,13 +485,15 @@ public:
                    XrdOucErrInfo    &out_error,
              const XrdSecEntity     *client = 0,
              const char             *opaque = 0) override {
-        UserSentry sentry(client, m_log);
+        UserSentry sentry(client, m_log, m_authz.get(), opaque);
         return m_sfs->truncate(Name, fileOffset, out_error, client, opaque);
     }
 
 private:
-    std::unique_ptr<XrdSfsFileSystem> m_sfs;
+    XrdSfsFileSystem *m_sfs;  // NOTE: we DO NOT own this pointer; given by the caller.  Do not make unique_ptr!
     XrdSysError m_log;
+    std::shared_ptr<XrdAccAuthorize> m_authz;
+    void *m_handle{nullptr};
 };
 
 extern "C" {
@@ -374,8 +503,11 @@ XrdSfsGetFileSystem(XrdSfsFileSystem *native_fs,
                     XrdSysLogger     *lp,
                     const char       *configfn)
 {
-    std::unique_ptr<XrdSfsFileSystem> sfs(native_fs);
-    return new MultiuserFileSystem(std::move(sfs), lp, configfn);
+    try {
+        return new MultiuserFileSystem(native_fs, lp, configfn);
+    } catch (std::runtime_error &) {
+        return nullptr;
+    }
 }
 
 }

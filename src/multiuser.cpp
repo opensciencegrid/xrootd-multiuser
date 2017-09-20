f8c42eb429eecb1e0410677639c17a63a6c6d2d8
Brian Bockelman <bbockelm@cse.unl.edu>
2017-09-20 14:56:56 -0500

    Set the appropriate capability per-thread.
    The effective capabilities need to be set per-thread; previously,
    this was only set per-process and the user-switching was only really
    functional when the request happened to be served from a lucky thread.

--- src/multiuser.cpp
+++ src/multiuser.cpp
@@ -35,6 +35,57 @@ static const int g_minimum_uid = 500;
 static const int g_minimum_gid = 500;
 
 
+static bool check_caps(XrdSysError &log) {
+    // See if we have the appropriate capabilities to run this plugin.
+    cap_t caps = cap_get_proc();
+    if (caps == NULL) {
+        log.Emsg("Initialize", "Failed to query xrootd daemon thread's capabilities", strerror(errno));
+        return false;
+    }
+    cap_value_t cap_list[2];
+    int caps_to_set = 0;
+    cap_flag_value_t test_flag = CAP_CLEAR;
+    // We must be at least permitted to acquire the needed capabilities.
+    cap_get_flag(caps, CAP_SETUID, CAP_PERMITTED, &test_flag);
+    if (test_flag == CAP_CLEAR) {
+        log.Emsg("check_caps", "CAP_SETUID not in xrootd daemon's permitted set");
+        cap_free(caps);
+        return false;
+    }
+    cap_get_flag(caps, CAP_SETGID, CAP_PERMITTED, &test_flag);
+    if (test_flag == CAP_CLEAR) {
+       log.Emsg("check_caps", "CAP_SETGID not in xrootd daemon's permitted set");
+        cap_free(caps);
+       return false;
+    }
+
+    // Determine which new capabilities are needed to be added to the effective set.
+    cap_get_flag(caps, CAP_SETUID, CAP_EFFECTIVE, &test_flag);
+    if (test_flag == CAP_CLEAR) {
+        //log.Emsg("Initialize", "Will request effective capability for CAP_SETUID");
+        cap_list[caps_to_set] = CAP_SETUID;
+        caps_to_set++;
+    }
+    cap_get_flag(caps, CAP_SETGID, CAP_EFFECTIVE, &test_flag);
+    if (test_flag == CAP_CLEAR) {
+        //log.Emsg("Initialize", "Will request effective capability for CAP_SETGID");
+        cap_list[caps_to_set] = CAP_SETGID;
+        caps_to_set++;
+    }
+    if (caps_to_set && cap_set_flag(caps, CAP_EFFECTIVE, caps_to_set, cap_list, CAP_SET) == -1) {
+        log.Emsg("Initialize", "Failed to add capabilities to the requested list.");
+        cap_free(caps);
+        return false;
+    }
+    if (caps_to_set && (cap_set_proc(caps) == -1)) {
+        log.Emsg("Initialize", "Failed to acquire necessary capabilities for thread");
+        cap_free(caps);
+        return false;
+    }
+    cap_free(caps);
+    return true;
+}
+
 class UserSentry {
 public:
     UserSentry(const XrdSecEntity *client, XrdSysError &log, XrdAccAuthorize *authz, const char *opaque) :
@@ -82,6 +133,10 @@ public:
         if (pwd.pw_gid < g_minimum_gid) {
             m_log.Emsg("UserSentry", "Username", client->name, "maps to a system GID; rejecting lookup");
             return;
+        }
+
+        if (!check_caps(m_log)) {
+            m_log.Emsg("UserSentry", "Unable to get correct capabilities for this thread - filesystem action likely to fail.");
         }
 
         m_log.Emsg("UserSentry", "Switching FS uid for user", client->name);
@@ -520,53 +575,10 @@ XrdSfsGetFileSystem(XrdSfsFileSystem *native_fs,
                     const char       *configfn)
 {
     XrdSysError log(lp, "multiuser_");
-    // See if we have the appropriate capabilities to run this plugin.
-    cap_t caps = cap_get_proc();
-    if (caps == NULL) {
-        log.Emsg("Initialize", "Failed to query xrootd daemon's capabilities", strerror(errno));
-        return nullptr;
-    }
-    cap_value_t cap_list[2];
-    int caps_to_set = 0;
-    cap_flag_value_t test_flag = CAP_CLEAR;
-    // We must be at least permitted to acquire the needed capabilities.
-    cap_get_flag(caps, CAP_SETUID, CAP_PERMITTED, &test_flag);
-    if (test_flag == CAP_CLEAR) {
-        log.Emsg("Initialize", "CAP_SETUID not in xrootd daemon's permitted set; unable to start multiuser plugin");
-        cap_free(caps);
-        return nullptr;
-    }
-    cap_get_flag(caps, CAP_SETGID, CAP_PERMITTED, &test_flag);
-    if (test_flag == CAP_CLEAR) {
-       log.Emsg("Initialize", "CAP_SETGID not in xrootd daemon's permitted set; unable to start multiuser plugin");
-        cap_free(caps);
-       return nullptr;
-    }
 
-    // Determine which new capabilities are needed to be added to the effective set.
-    cap_get_flag(caps, CAP_SETUID, CAP_EFFECTIVE, &test_flag);
-    if (test_flag == CAP_CLEAR) {
-        log.Emsg("Initialize", "Will request effective capability for CAP_SETUID");
-        cap_list[caps_to_set] = CAP_SETUID;
-        caps_to_set++;
-    }
-    cap_get_flag(caps, CAP_SETGID, CAP_EFFECTIVE, &test_flag);
-    if (test_flag == CAP_CLEAR) {
-        log.Emsg("Initialize", "Will request effective capability for CAP_SETGID");
-        cap_list[caps_to_set] = CAP_SETGID;
-        caps_to_set++;
-    }
-    if (cap_set_flag(caps, CAP_EFFECTIVE, caps_to_set, cap_list, CAP_SET) == -1) {
-        log.Emsg("Initialize", "Failed to add capabilities to the requested list.");
-        cap_free(caps);
+    if (!check_caps(log)) {
         return nullptr;
     }
-    if (cap_set_proc(caps) == -1) {
-        log.Emsg("Initialize", "Failed to acquire necessary capabilities to start multiuser plugin");
-        cap_free(caps);
-        return nullptr;
-    }
-    cap_free(caps);
 
     try {
         return new MultiuserFileSystem(native_fs, lp, configfn);
